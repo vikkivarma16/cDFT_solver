00ab952735d81c1d7fdc8ff8a8df5bb255903118
Your Name <vikkivarma16@gmail.com>
2025-12-29 13:44:58 +0200

    update_data_2025-12-29_13-44-58

--- cdft_solver/generators/parameters/dictionary.py
+++ cdft_solver/generators/parameters/dictionary.py
@@ -2,7 +2,7 @@ from .pair_interactions import pair_interactions
 from .space import space
 
 __all__ = [
-    "particles_interactions",
+    "pair_interactions",
     "box_parameters",
     "confinement_parameters",
 ]
