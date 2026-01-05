ab2820b0250d26657c63e7b3702fb03428f4a410
Your Name <vikkivarma16@gmail.com>
2026-01-05 02:33:23 +0200

    update_data_2026-01-05_02-33-23

--- cdft_solver/calculators/radial_distribution_function/utils.py
+++ cdft_solver/calculators/radial_distribution_function/utils.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 
-def safe_exp(x, xmin=-50.0, xmax=50.0):
+def safe_exp(x, xmin=-500.0, xmax=500.0):
     """
     Numerically safe exponential.
     Clips exponent argument before applying exp.
