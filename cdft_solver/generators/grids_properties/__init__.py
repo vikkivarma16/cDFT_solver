a1b1e308ee7bf26a1c55dd3c6b5988d0573bfb68
Your Name <vikkivarma16@gmail.com>
2026-01-05 23:49:46 +0200

    update_data_2026-01-05_23-49-46

--- cdft_solver/generators/grids_properties/__init__.py
+++ cdft_solver/generators/grids_properties/__init__.py
@@ -8,6 +8,6 @@ for running isochores DFT minimization.
 
 from .bulk_rho_mue_z_space_box import bulk_rho_mue_z_space_box
 from .k_and_r_space_box import r_k_space_box
-from .k_and_r_space_cylinder import r_k_space_cylindrical
+from .k_and_r_space_cylindrical import r_k_space_cylindrical
 from .k_and_r_space_spherical import r_k_space_spherical
 from .external_potential_grid import external_potential_grid
