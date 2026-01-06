7a13f65853fc0ef6f1a467215d58f41bb9c6b044
Your Name <vikkivarma16@gmail.com>
2026-01-06 23:10:49 +0200

    update_data_2026-01-06_23-10-49

--- cdft_solver/calculators/one_d_profile_iterator/one_d_profile_iterator_box.py
+++ cdft_solver/calculators/one_d_profile_iterator/one_d_profile_iterator_box.py
@@ -775,6 +775,9 @@ def one_d_profile_iterator_box(ctx, config, export_json= True, export_plots = Tr
         or True  # symmetry depends on how you filled it
 
     dz = x[1] - x[0]
+    
+    print (vij_array)
+    
 
 
     while (iteration < iteration_max):
