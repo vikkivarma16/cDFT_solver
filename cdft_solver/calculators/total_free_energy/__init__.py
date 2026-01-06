5414dda30408b70b87501e2746a7c6c2a0a2e4b4
Your Name <vikkivarma16@gmail.com>
2026-01-06 15:40:31 +0200

    update_data_2026-01-06_15-40-31

--- cdft_solver/calculators/total_free_energy/__init__.py
+++ cdft_solver/calculators/total_free_energy/__init__.py
@@ -8,5 +8,4 @@ for running isochores DFT minimization.
 
 # Import all custom pair potentials
 from .total_free_energy import total_free_energy
-from .total_free_energy_planer import total_free_energy_planer
 from .free_energy_exporter  import free_energy_exporter
