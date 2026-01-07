e18756d1911047408e97953ca45ca1d07c051560
Your Name <vikkivarma16@gmail.com>
2026-01-07 14:32:12 +0200

    update_data_2026-01-07_14-32-12

--- cdft_solver/calculators/radial_distribution_function/rdf_planer.py
+++ cdft_solver/calculators/radial_distribution_function/rdf_planer.py
@@ -119,10 +119,10 @@ def inverse_hankel_transform_2d(F_k, r, k_grid):
 # -------------------------------------------------
 
 
-def solve_oz_matrix_2d(c_r_matrix, RHO, r_grid, k_grid, J0, Ns, Nz, Nr):   
+def solve_oz_matrix_2d(c_r, RHO, r_grid, k_grid, J0, Ns, Nz, Nr):   
     # Flatten i,j
     Nd = Ns * Nz
-    c_flat = c_r_matrix.reshape(Ns, Ns, Nz*Nz, Nr)
+    c_flat = c_r.reshape(Ns, Ns, Nz*Nz, Nr)
     gamma_flat = np.zeros_like(c_flat)
 
     # Vectorized Hankel transform
