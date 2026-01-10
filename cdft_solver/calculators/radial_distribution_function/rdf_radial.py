8541d15a3f5f0b304952a148d2fbd6d0b37cc6bb
Your Name <vikkivarma16@gmail.com>
2026-01-10 20:42:10 +0200

    update_data_2026-01-10_20-42-10

--- cdft_solver/calculators/radial_distribution_function/rdf_radial.py
+++ cdft_solver/calculators/radial_distribution_function/rdf_radial.py
@@ -587,7 +587,7 @@ def rdf_radial(
     n_points = rdf_block.get("n_points", 300)
     r_max  =  rdf_block.get("r_max", 6)
     dr = r_max / (n_points + 1)
-    r = dr * np.arange(1, n_points + 1)
+    r = dr * np.arange(2, n_points + 2)
     
     print (r)
     print ((n_points+1 )*dr)
