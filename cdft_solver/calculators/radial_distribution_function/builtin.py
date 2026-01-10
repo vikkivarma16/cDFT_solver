d8bb9c0c46a1e6c2866c800e6d9ef39944f6a806
Your Name <vikkivarma16@gmail.com>
2026-01-10 19:50:48 +0200

    update_data_2026-01-10_19-50-48

--- cdft_solver/calculators/radial_distribution_function/builtin.py
+++ cdft_solver/calculators/radial_distribution_function/builtin.py
@@ -6,21 +6,14 @@ from .utils import safe_exp
 
 def py_closure(r, gamma, u, sigma_ab=None):
     #print("its me running")
-    return (1.0 + gamma) * (safe_exp(- u) - 1.0)
+    return (1.0 + gamma) * (safe_exp(-u) - 1.0)
 
 
 def hnc_closure(r, gamma, u, sigma_ab=None):
     #print("HI I am being accessed")
     
-    with np.errstate(over='ignore', invalid='ignore'):
-        # attempt the usual closure
-        value = np.exp(- u + gamma) - gamma - 1.0
-
-    # handle NaNs or infs due to overflow
-    mask = ~np.isfinite(value)  # True for NaN or inf
-    if np.any(mask):
-        # linearized approximation: exp(gamma) ≈ 1 + gamma
-        value = (1 + gamma[mask]) * (np.exp(-  u[mask]) - 1)
+    
+    value = safe_exp(-u + gamma) - gamma - 1.0
     
     return value
 
--- cdft_solver/calculators/radial_distribution_function/utils.py
+++ cdft_solver/calculators/radial_distribution_function/utils.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 
-def safe_exp(x, xmin=-50000.0, xmax=50000.0):
+def safe_exp(x, xmin=-50000000.0, xmax=5000000.0):
     """
     Numerically safe exponential.
     Clips exponent argument before applying exp.
