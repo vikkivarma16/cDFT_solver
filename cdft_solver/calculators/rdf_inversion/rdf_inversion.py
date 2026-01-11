8801664a02e6d032a1119fd10539a081b1d06589
Your Name <vikkivarma16@gmail.com>
2026-01-11 13:13:25 +0200

    update_data_2026-01-11_13-13-25

--- cdft_solver/calculators/rdf_inversion/rdf_inversion.py
+++ cdft_solver/calculators/rdf_inversion/rdf_inversion.py
@@ -8,10 +8,14 @@ from collections import defaultdict
 from pathlib import Path
 import matplotlib.pyplot as plt
 import re
-from .closer import closure_update_c_matrix
 from scipy.optimize import minimize_scalar
 
+from collections.abc import Mapping
+from cdft_solver.generators.potential_splitter.hc import hard_core_potentials 
+from cdft_solver.generators.potential_splitter.mf import meanfield_potentials 
+from cdft_solver.generators.potential_splitter.total import total_potentials
 
+from cdft_solver.calculators.radial_distribution_function.closure import closure_update_c_matrix
 
 
 # -----------------------------
