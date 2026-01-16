d2f1da4b166a9cc966a78851d3a7659cea31db5f
Your Name <vikkivarma16@gmail.com>
2026-01-16 09:01:13 +0200

    update_data_2026-01-16_09-01-13

--- cdft_solver/calculators/rdf_inversion/__init__.py
+++ cdft_solver/calculators/rdf_inversion/__init__.py
@@ -9,3 +9,5 @@ for running isochores DFT minimization.
 from .rdf_inversion import boltzmann_inversion
 
 from .rdf_inversion_advanced import boltzmann_inversion_advanced
+
+from .rdf_inversion_standard import boltzmann_inversion_standard
