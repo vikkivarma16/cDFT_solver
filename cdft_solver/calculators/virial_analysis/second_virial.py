68c6f6f09ae013a821fa60a097d7b89a8262730f
Your Name <vikkivarma16@gmail.com>
2026-01-22 13:17:12 +0200

    update_data_2026-01-22_13-17-12

--- cdft_solver/calculators/virial_analysis/second_virial.py
+++ cdft_solver/calculators/virial_analysis/second_virial.py
@@ -147,7 +147,7 @@ def second_virial(
             # ---- Integrated strength (thermodynamic integration)
             # g_lambda(r) = exp(-lambda * beta u)
             gl = np.exp(-lam[:, None] * uij[None, :])
-            lambda_integrand = 1 * uij[None, :]
+            lambda_integrand = gl * uij[None, :]
             lambda_integrated = np.trapz(lambda_integrand, lam, axis=0)
 
             strength_integrand = 4.0 * np.pi * r**2 * lambda_integrated
