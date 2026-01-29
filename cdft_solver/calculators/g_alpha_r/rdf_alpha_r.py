76c08c529a1ad1fcdd48bae1f6caeff133c7d890
Your Name <vikkivarma16@gmail.com>
2026-01-29 21:30:10 +0000

    update_data_2026-01-29_21-30-10

--- cdft_solver/calculators/g_alpha_r/rdf_alpha_r.py
+++ cdft_solver/calculators/g_alpha_r/rdf_alpha_r.py
@@ -670,7 +670,7 @@ def rdf_alpha_r(
     # -----------------------------
     
     n_points = rdf_block.get("n_points", 300)
-    r_max  =  rdf_block.get("r_max", 6)
+    r_max  =  rdf_block.get("r_max", 4)
     dr = r_max / (n_points + 1)
     r = dr * np.arange(1, n_points + 1)
     
@@ -681,6 +681,9 @@ def rdf_alpha_r(
     pair_closures = np.empty((N, N), dtype=object)
 
     n = len(species)
+    
+    Nr =  n_points 
+    
     pair_closures = np.empty((n, n), dtype=object)
 
     for i, si in enumerate(species):
