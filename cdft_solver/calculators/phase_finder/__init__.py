75528b2da1b90faa0af4aa8d25e84c5b56573df9
Your Name <vikkivarma16@gmail.com>
2026-02-22 19:09:57 +0200

    update_data_2026-02-22_19-09-57

--- cdft_solver/calculators/phase_finder/__init__.py
+++ cdft_solver/calculators/phase_finder/__init__.py
@@ -6,4 +6,5 @@ this module compute the possible number of phases
 
 
 
-from .phase_finder import scan_isochem_multi
+from .phase_finder_isochem import scan_isochem_multi
+from .phase_finder_isocore import scan_isocore_multi
