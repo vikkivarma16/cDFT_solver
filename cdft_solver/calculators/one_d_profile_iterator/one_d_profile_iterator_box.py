c18eaa1787216bc4da9522a462d2c11c080dc4a2
Your Name <vikkivarma16@gmail.com>
2026-03-03 10:05:23 +0200

    update_data_2026-03-03_10-05-23

--- cdft_solver/calculators/one_d_profile_iterator/one_d_profile_iterator_box.py
+++ cdft_solver/calculators/one_d_profile_iterator/one_d_profile_iterator_box.py
@@ -1293,7 +1293,7 @@ def one_d_profile_iterator_box(ctx, config, export_json= True, export_plots = Tr
             
             
             print(vij_point)
-            print(pressur_values[i])
+            print(pressure_values[i])
             print(bulk_pressure)
                 
                 
