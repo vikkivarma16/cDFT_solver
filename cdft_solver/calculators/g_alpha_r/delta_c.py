1bc46cfd8de3c0594f0e494e42fbdf0aa175f219
Your Name <vikkivarma16@gmail.com>
2026-03-29 14:26:52 +0300

    update_data_2026-03-29_14-26-52

--- cdft_solver/calculators/g_alpha_r/delta_c.py
+++ cdft_solver/calculators/g_alpha_r/delta_c.py
@@ -801,7 +801,7 @@ def delta_c_alpha(
 
     c_ref, gamma_ref, g_ref, conversed = multi_component_oz_solver_alpha(
         r=r,
-        pair_closures=pair_closures
+        pair_closures=pair_closures,
         densities=densities_step,
         u_matrix=u_matrix,
         sigma_matrix=sigma_matrix,
